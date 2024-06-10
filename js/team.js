3892b83bbe2a24b81053452c5cd498588e33d784
Server <jaredstef@useres.noreply.github.com>
2024-06-10 18:33:38 -0400

    Updated NHL Power Rankings 2024-06-10 18:33:37

---
+++ js/team.js
@@ -0,0 +1,110 @@
+window.onload = function() {
+    var ctx = document.getElementById('myChart');
+    ctx.height = 600; // Set the height as needed
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                data: {{Elos}},
+                borderColor: '#3935DA',
+                fill: false,
+                pointRadius: 0,
+                borderWidth: 2 // Adjust as needed
+            },
+            {
+                label: 'Min',
+                data: {{Mins}},
+                borderColor: 'gray',
+                fill: '+1',
+                pointRadius: 0,
+                borderWidth: 1 // Adjust as needed
+            },
+            {
+                label: 'Max',
+                data: {{Maxs}},
+                borderColor: 'gray',
+                fill: false,
+                pointRadius: 0,
+                borderWidth: 1 // Adjust as needed
+            }
+        ]
+        },
+        options: {
+            hover: {
+                mode: null
+            },
+            animation: false,
+            maintainAspectRatio: false, // Add this line
+            scales: {
+                y: {
+                    min: 1300,
+                    max: 1700,
+                    ticks: {
+                        color: 'black',
+                        font: {
+                            size: 20, // Adjust as needed
+                            family:  "Kumbh Sans"
+                        },
+                        callback: function(value, index, values) {
+                            // Display only every other tick
+                            if (index % 2 === 0) return value;
+                        }
+                    }
+                },
+                x: {
+
+                    type: 'time',
+                        time: {
+                            unit: 'month'
+                        },
+                        ticks: {
+                            callback: function(value, index, values) {
+                                const date = new Date(value);
+                                const monthNames = ["January", "February", "March", "April", "May", "June",
+                                    "July", "August", "September", "October", "November", "December"
+                                ];
+                                return monthNames[date.getMonth()];
+                            },
+                            color: 'black',
+                        font: {
+                            size: 20, // Adjust as needed
+                            family:  "Kumbh Sans" // Adjust to match your body font
+                        },
+                        padding: 20,
+                        }
+                }
+            },
+            plugins: {
+                tooltip: {
+                    enabled: false
+                },
+                legend: {
+                    display: false
+                },
+                annotation: {
+                    annotations: {
+                        line2: {
+                            type: 'line',
+                            yMin: 1504.7,
+                            yMax: 1504.7,
+                            borderColor: 'black',
+                            borderWidth: 1,
+                            label: {
+                                content: 'NHL Avg',
+                                enabled: true,
+                                position: 'center',
+                                xAdjust: 70,
+                                backgroundColor: 'rgba(255, 255, 255, 0)', // Add this line
+                                color: 'black', // Add this line
+                                font: {
+                                    size: 13, // Adjust as needed
+                                    family: "Kumbh Sans", // Adjust to match your body font
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
